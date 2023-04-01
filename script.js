9d5e2b6e2614984471f13c04d6b642d7b2542485
rushibelkunde <rushibelkunde18@gmail.com>
2023-04-01 21:19:12 +0530

    intial commit

---
+++ script.js
@@ -0,0 +1,38 @@
+
+
+var skill = document.getElementsByClassName("skill");
+
+
+
+
+document.addEventListener("mouseover",(e)=>{
+
+    
+    if(e.target.className=="skill"){
+        e.target.style="animation-name: animation;animation-duration:4s;animation-iteration-count: normal;animation-direction: alternate-reverse;"
+    }
+
+    setTimeout(()=>{
+        e.target.style=""
+
+    },8000)
+})
+
+
+const observer = new IntersectionObserver((entries)=>{
+    entries.forEach((entry)=>{
+
+        if(entry.isIntersecting){
+            console.log("xyz")
+            entry.target.classList.add("show")
+        }
+        else{
+            entry.target.classList.remove("show")
+        }
+    })
+})
+
+const hiddenElements = document.querySelectorAll(".hidden");
+
+
+hiddenElements.forEach((el)=>observer.observe(el));
